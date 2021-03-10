8c889ac326b7df8bb8087ef18ef7bc7ae96f1fcb
hurjaehyeok <Jack2ee.dev@gmail.com>
2021-03-10 19:13:32 +0900

    fix: change LinkedListImplTest to importing * from Assertions from importing each package

--- src/test/java/datastructure/linkedList/LinkedListImplTest.java
+++ src/test/java/datastructure/linkedList/LinkedListImplTest.java
@@ -5,11 +5,7 @@ import org.junit.jupiter.api.Test;
 import datastructure.linkedList.LinkedListImpl.Node;
 import datastructure.linkedList.LinkedListImpl.NodeMgmt;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class LinkedListImplTest {
 
